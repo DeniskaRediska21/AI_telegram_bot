1890db9b8fd59dc195a208bc8307fdfc9f2b2c6b
DeniskaRediska21 <denisgontar99@gmail.com>
2023-11-03 11:56:56 +0300

    mwp reached

--- telebot_bot.py
+++ telebot_bot.py
@@ -12,7 +12,7 @@ bot = telebot.TeleBot(config.BOT_TOKEN)
 lang = 'en'
 
 @bot.message_handler(commands = ['lang'])
-def lang(language):
+def lang_process(language):
     line = language.text
     lang = line[line.find('/')+line[line.find('/')+1:].strip().find(' ')+1:].strip() 
     bot.send_message(language.from_user.id, f"lang set to: {lang}")
@@ -21,19 +21,25 @@ def lang(language):
 def gettext(message):
     try:
 
+        bot.send_message(message.from_user.id, "Минуточку...")
+
         rp = RegisteredProviders()
         rp.parse_providers()
         proxi = rp.get_random_proxy()
-        bot.send_message(message.from_user.id, "Минуточку...")
 
-        en_txt = translator.translate(message.text,dest = lang) 
+
+        en_txt = translator.translate(message.text,dest = 'en') 
 
         ans = g4f.ChatCompletion.create(proxi=proxi,model = "gpt-3.5-turbo",messages = [{"role":"user","content":en_txt.text}])
-
-        ans_ru = translator.translate(ans,dest = 'en').text  
+        try:
+            ans_ru = translator.translate(ans,dest = lang).text
+        except:
+            lang = 'en'
+            ans_ru = translator.translate(ans,dest = lang).text
+        
 
         bot.send_message(message.from_user.id, ans_ru)
-    except:
+   except:
         bot.send_message(message.from_user.id, "Что-то пошло не так :( Повторите запрос")
 
         
