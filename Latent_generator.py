5fca2a5c93c1dd998334bd76280008d82163fa9d
DeniskaRediska21 <root@localhost>
2023-11-24 12:01:08 +0000

    Trying to fix seed not working

--- Latent_generator.py
+++ Latent_generator.py
@@ -1,11 +1,17 @@
 import torch
+import np
+import random
 
 def generate_latents(pipe, height=512, width=512, seed = 0,generator=torch.Generator()):
     latents = None
     #Get a new random seed, store it and use it as the generator state
     if seed == 0:
         seed = generator.seed()
-    
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic=True
+        
     generator = generator.manual_seed(seed)
     image_latents = torch.randn(
     (1, pipe.unet.in_channels, height // 8, width // 8),
